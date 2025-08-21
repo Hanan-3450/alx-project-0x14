dc6679e416e7f4b4bce13a3767ef164b74435e99
Hanan <your-email@example.com>
2025-08-21 06:00:10 -0700

    Implement Task 1: Add layout, header, footer, button component, and landing page setup

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,19 +1,15 @@
-import React ,{ ReactNode} from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import { ComponentProps } from "../../interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
 
-interface LayoutProps {
-    children: ReactNode;
-}
-
-const Layout: React.FC<LayoutProps> = ({  children }) => {
-    return (
-        <div>
-            <Header/>
-            <main>{children}</main>
-            <Footer/>
-        </div>
-    );
+const Layout: React.FC<ComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
